c7449deed017d6b66378031735325ece5ed22288
Anthony Kuske <anthonykuske@gmail.com>
2020-03-31 21:31:02 +0100

    Add analytics

--- src/frontend/js/app.js
+++ src/frontend/js/app.js
@@ -1,10 +1,16 @@
 import Vue from 'vue';
+import VueAnalytics from 'vue-analytics';
 import App from './App.vue';
 import { createRouter } from '@frontend/router';
 import { createStore } from '@frontend/store';
 
 const router = createRouter();
 const store = createStore();
+
+Vue.use(VueAnalytics, {
+    id: process.env.ANALYTICS_ID,
+    router
+});
 
 const app = new Vue({
     router,
